463de8b9df6a3d0aa73aef3afdcb8e6b1403dc4b
Yurkapur <yurkapur83@gmail.com>
2016-09-14 20:35:13 -0700

    Add method for locating any element in list

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,4 +15,24 @@ end
 Appium::Driver.new(caps)
 Appium.promote_appium_methods Object
 
+def find_element_in_list(text)
+	puts(get_source)
+	4.times{Appium::TouchAction.new.swipe(start_x: 0.5, start_y: 0.2, end_x: 0.5, end_y: 0.8, duration: 600).perform}
+	
+	current_screen = get_source
+	previous_screen = ""
+
+  	until (exists{text("#{text}")})||(current_screen == previous_screen) do 
+    	Appium::TouchAction.new.swipe(start_x: 0.5, start_y: 0.8, end_x: 0.5, end_y: 0.2, duration: 600).perform
+  		previous_screen = current_screen
+  		current_screen = get_source
+  	end
+
+  	unless exists{text"#{text}"}    # the same as:   if !exists{text"#{text}"}
+  		fail("Element with name #{text} not found")
+  	}
+  end
+end
+
+
 
